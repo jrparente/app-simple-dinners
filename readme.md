eff7a4365b437540924ea781b7ce3dce155868bb
jrparente <jrparente@gmail.com>
2022-12-10 13:53:14 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Simple Meals
+
+A simple app that helps me get dinner ideas and generate shopping lists.
+
+## Features
+
+- Store ingredients in arrays ✅
+- Select Random ingredients from a meal ✅
+- Add and select entire meals alongside random ingredients
+- Generate a shopping list from all the required produce
+- Render results in the DOM
+
+## Roadmap
+
+### Short / Medium term:
+
+- Ability for the user to add ingredients to the arrays (ie: most used ingredients in a household)
+- Add cooking method ideas (ie: steemed whitefish)
+- Add images to ingredients? And maybe prefered cooking methods for each ingredient?
+
+### Future:
+
+- Light/dark mode toggle
+- Ability to store information in a database
---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+// 📝 To-dos:
+// Store ingredients in arrays ✅
+// Select Random ingredients from a meal ✅
+// Add and select entire meals alongside random ingredients
+// Generate a shopping list from all the required produce
+// Render results in the DOM
+
+function App() {
+  const proteins = [
+    "chicken",
+    "codfish",
+    "whitefish",
+    "canned tuna",
+    "tuna steak",
+  ];
+  const carbs = [
+    "boiled potatoes",
+    "french fries",
+    "pasta",
+    "noisettes",
+    "rice",
+  ];
+  const vegs = [
+    "broccoli",
+    "tomatoes",
+    "carrots",
+    "cauliflower",
+    "bell peppers",
+  ];
+  const readyMeals = ["spaghetti carbonara", "pizza", "hamburguer"];
+
+  function randomIngredient(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  let [dinnerPlate, setDinnerPlate] = useState([]);
+
+  function getDinners() {
+    setDinnerPlate([
+      randomIngredient(proteins),
+      randomIngredient(carbs),
+      randomIngredient(vegs),
+      randomIngredient(vegs),
+      randomIngredient(vegs),
+    ]);
+    console.log(dinnerPlate);
+  }
+
+  return (
+    <div className="App container">
+      <p>Dinners:</p>
+      <p>Shopping List:</p>
+      <button onClick={getDinners}>Give me the Dinners</button>
+    </div>
+  );
+}
+
+export default App;
