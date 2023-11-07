27b09db4e1e6b2a12d0d944cc60b5b93f3e1a3c3
Joana Parente <jrparente@gmail.com>
2023-11-07 10:10:56 +0000

    .

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,6 +1,6 @@
 import { Loader2 } from "lucide-react";
 import React from "react";
-import styles from "./Loading.module.css";
+import styles from "./loading.module.css";
 
 function Loading() {
   return (
