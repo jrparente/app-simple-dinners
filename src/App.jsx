b910e755973d467388b65fab9bf0d3eb661fa596
Joana Parente <jrparente@gmail.com>
2023-11-20 16:24:56 +0000

    Fixed sidebar in mobile and desktop views

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,28 @@ import RecipePage from "./pages/(dashboard)/Recipes/RecipePage/[id]";
 import EditRecipe from "./pages/(dashboard)/Recipes/EditRecipe";
 import Dashboard from "./pages/(dashboard)/Dashboard/Dashboard";
 import Header from "./pages/(marketing)/_components/Header/Header";
+import { useState } from "react";
+import { ChevronsRight } from "lucide-react";
 
 function App() {
   const [cookies, setCookies] = useCookies(["access_token"]);
+  const isMobile = window.innerWidth < 768;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const collapseSidebar = () => {
+    console.log("clicked collapseSidebar");
+    if (isSidebarOpen) {
+      setIsSidebarOpen(false);
+    } else {
+      setIsSidebarOpen(true);
+    }
+  };
+
+  const resetSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  console.log("isSidebarOpen", isSidebarOpen);
 
   return (
     <div className="App">
@@ -30,9 +49,29 @@ function App() {
             <Footer />
           </>
         ) : (
-          <>
-            <Sidebar />
-            <div className="main-content">
+          <div className="flex w-full" style={{ overflowX: "hidden" }}>
+            {isSidebarOpen && (
+              <Sidebar
+                isMobile={isMobile}
+                isSidebarOpen={isSidebarOpen}
+                collapseSidebar={collapseSidebar}
+              />
+            )}
+            <div
+              className="main-content"
+              style={{ marginLeft: isSidebarOpen ? "20rem" : "0" }}
+            >
+              <header className="w-full flex">
+                {!isSidebarOpen && (
+                  <div
+                    role="button"
+                    onClick={() => resetSidebar()}
+                    className="iconButton"
+                  >
+                    <ChevronsRight />
+                  </div>
+                )}
+              </header>
               <Routes>
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/recipe/:recipeId" element={<RecipePage />} />
@@ -43,7 +82,7 @@ function App() {
               </Routes>
               <Footer />
             </div>
-          </>
+          </div>
         )}
       </BrowserRouter>
     </div>
